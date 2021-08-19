5e927fae90c7ae7ab899eb63ee2c51cd78aeaf1c
yamazaki hiroki <yamazaki.hiroki.58r@gmail.com>
2021-08-19 20:48:53 +0900

    directed degree constraint

--- src/digraphillion/spec/FrontierDegreeSpecified.hpp
+++ src/digraphillion/spec/FrontierDegreeSpecified.hpp
@@ -5,6 +5,9 @@
 #include <vector>
 
 #include "FrontierData.hpp"
+#include "FrontierManager.hpp"
+#include "subsetting/DdSpec.hpp"
+#include "subsetting/util/Digraph.hpp"
 #include "subsetting/util/IntSubset.hpp"
 
 using namespace tdzdd;
@@ -12,11 +15,11 @@ using namespace tdzdd;
 typedef unsigned char uchar;
 
 class FrontierDegreeSpecifiedSpec
-    : public tdzdd::PodArrayDdSpec<FrontierDegreeSpecifiedSpec, DirectedFrontierData,
-                                   2> {
+    : public tdzdd::PodArrayDdSpec<FrontierDegreeSpecifiedSpec,
+                                   DirectedFrontierData, 2> {
  private:
   // input graph
-  const tdzdd::Graph& graph_;
+  const tdzdd::Digraph& graph_;
   // number of vertices
   const int n_;
   // number of edges
@@ -24,44 +27,41 @@ class FrontierDegreeSpecifiedSpec
 
   const FrontierManager fm_;
 
-  const std::vector<IntSubset*> degRanges_;
+  std::vector<IntSubset const*> in_constraints;
+  std::vector<IntSubset const*> out_constraints;
 
   // This function gets deg of v.
-  int getDeg(FrontierDSData* data, int v) const {
-    return static_cast<int>(data[fm_.vertexToPos(v) * 2]);
+  short getIndeg(DirectedFrontierData* data, short v) const {
+    return data[fm_.vertexToPos(v)].indeg;
+  }
+
+  short getOutdeg(DirectedFrontierData* data, short v) const {
+    return data[fm_.vertexToPos(v)].outdeg;
   }
 
   // This function sets deg of v to be d.
-  void setDeg(FrontierDSData* data, int v, int d) const {
-    data[fm_.vertexToPos(v) * 2] = static_cast<uchar>(d);
+  void setIndeg(DirectedFrontierData* data, short v, short d) const {
+    data[fm_.vertexToPos(v)].indeg = d;
   }
 
-  int getDegUpper(FrontierDSData* data) const {
-    int deg;
-    for (deg = static_cast<int>(degRanges_.size()) - 1; deg >= 0; --deg) {
-      if (data[fixedDegStart_ + deg] < degRanges_[deg]->upperBound()) {
-        break;
-      }
-    }
-    return deg;
+  void setOutdeg(DirectedFrontierData* data, short v, short d) const {
+    data[fm_.vertexToPos(v)].outdeg = d;
   }
 
-  void initializeData(FrontierDSData* data) const {
-    for (int i = 0; i < fixedDegStart_ + static_cast<int>(degRanges_.size());
-         ++i) {
-      data[i] = 0;
+  void initializeData(DirectedFrontierData* data) const {
+    for (int i = 0; i < fm_.getMaxFrontierSize(); ++i) {
+      data[i].indeg = 0;
+      data[i].outdeg = 0;
+      data[i].comp = 0;
     }
   }
 
  public:
-  FrontierDegreeSpecifiedSpec(const tdzdd::Graph& graph,
-                              const std::vector<IntSubset*>& degRanges)
+  FrontierDegreeSpecifiedSpec(const tdzdd::Digraph& graph)
       : graph_(graph),
         n_(static_cast<short>(graph_.vertexSize())),
         m_(graph_.edgeSize()),
-        fm_(graph_),
-        fixedDegStart_(fm_.getMaxFrontierSize() * 2),
-        degRanges_(degRanges) {
+        fm_(graph_) {
     if (graph_.vertexSize() > SHRT_MAX) {  // SHRT_MAX == 32767
       std::cerr << "The number of vertices should be at most " << SHRT_MAX
                 << std::endl;
@@ -70,31 +70,49 @@ class FrontierDegreeSpecifiedSpec
 
     // todo: check all the degrees is at most 256
 
-    setArraySize(fixedDegStart_ + degRanges_.size());
+    setArraySize(fm_.getMaxFrontierSize());
+    int m = graph.vertexSize();
+    in_constraints.resize(m + 1);
+    out_constraints.resize(m + 1);
+    for (int v = 1; v <= m; v++) {
+      in_constraints[v] = 0;
+      out_constraints[v] = 0;
+    }
   }
 
-  int getRoot(FrontierDSData* data) const {
+  void setIndegConstraint(Digraph::VertexNumber v, IntSubset const* c) {
+    if (v < 1 || graph_.vertexSize() < v)
+      throw std::runtime_error("ERROR: Vertex number is out of range");
+    in_constraints[v] = c;
+  }
+
+  void setOutdegConstraint(Digraph::VertexNumber v, IntSubset const* c) {
+    if (v < 1 || graph_.vertexSize() < v)
+      throw std::runtime_error("ERROR: Vertex number is out of range");
+    out_constraints[v] = c;
+  }
+
+  int getRoot(DirectedFrontierData* data) const {
     initializeData(data);
     return m_;
   }
 
-  int getChild(FrontierDSData* data, int level, int value) const {
+  int getChild(DirectedFrontierData* data, int level, int value) const {
     assert(1 <= level && level <= m_);
-
-    // std::cerr << "level = " << level << ", value = " << value << std::endl;
 
     // edge index (starting from 0)
     const int edge_index = m_ - level;
     // edge that we are processing.
     // The endpoints of "edge" are edge.v1 and edge.v2.
-    const Graph::EdgeInfo& edge = graph_.edgeInfo(edge_index);
+    const Digraph::EdgeInfo& edge = graph_.edgeInfo(edge_index);
 
-    // initialize deg and comp of the vertices newly entering the frontier
+    // initialize deg of the vertices newly entering the frontier
     const std::vector<int>& entering_vs = fm_.getEnteringVs(edge_index);
     for (size_t i = 0; i < entering_vs.size(); ++i) {
       int v = entering_vs[i];
       // initially the value of deg is 0
-      setDeg(data, v, 0);
+      setIndeg(data, v, 0);
+      setOutdeg(data, v, 0);
     }
 
     // vertices on the frontier
@@ -102,16 +120,16 @@ class FrontierDegreeSpecifiedSpec
 
     if (value == 1) {  // if we take the edge (go to 1-arc)
       // increment deg of v1 and v2 (recall that edge = {v1, v2})
-
-      int upper = getDegUpper(data);
-      if (getDeg(data, edge.v1) + 1 > upper) {
+      auto outdeg1 = getOutdeg(data, edge.v1);
+      if (!out_constraints[edge.v1]->contains(outdeg1 + 1)) {
         return 0;
       }
-      if (getDeg(data, edge.v2) + 1 > upper) {
+      auto indeg2 = getIndeg(data, edge.v2);
+      if (!in_constraints[edge.v2]->contains(indeg2)) {
         return 0;
       }
-      setDeg(data, edge.v1, getDeg(data, edge.v1) + 1);
-      setDeg(data, edge.v2, getDeg(data, edge.v2) + 1);
+      setIndeg(data, edge.v2, indeg2 + 1);
+      setOutdeg(data, edge.v1, outdeg1 + 1);
     }
 
     // vertices that are leaving the frontier
@@ -119,64 +137,16 @@ class FrontierDegreeSpecifiedSpec
     for (size_t i = 0; i < leaving_vs.size(); ++i) {
       int v = leaving_vs[i];
 
-      int d = getDeg(data, v);
-
-      bool samecomp_found = false;
-      bool nonisolated_found = false;
-
-      // Search a vertex that has the component number same as that of v.
-      // Also check whether a vertex whose degree is at least 1 exists
-      // on the frontier.
-      for (size_t j = 0; j < frontier_vs.size(); ++j) {
-        int w = frontier_vs[j];
-        if (w == v) {  // skip if w is the leaving vertex
-          continue;
-        }
-        // skip if w is one of the vertices that
-        // has already leaved the frontier
-        bool found_leaved = false;
-        for (size_t k = 0; k < i; ++k) {
-          if (w == leaving_vs[k]) {
-            found_leaved = true;
-            break;
-          }
-        }
-        if (found_leaved) {
-          continue;
-        }
-        // The degree of w is at least 1.
-        if (getDeg(data, w) > 0) {
-          nonisolated_found = true;
-        }
-        if (nonisolated_found && samecomp_found) {
-          break;
-        }
+      int indeg = getIndeg(data, v), outdeg = getOutdeg(data, v);
+      if (!in_constraints[v]->contains(indeg) ||
+          !out_constraints[v]->contains(outdeg)) {
+        return 0;
       }
-      // There is no vertex that has the component number
-      // same as that of v. That is, the connected component
-      // of v becomes determined.
-      if (!samecomp_found) {
-        // Check whether v is isolated.
-        // If v is isolated (deg of v is 0), nothing occurs.
-        if (d > 0) {
-          // Check whether there is a
-          // connected component other than that of v,
-          // that is, the generated subgraph is not connected.
-          // If so, we return the 0-terminal.
-          if (nonisolated_found) {
-            return 0;  // return the 0-terminal.
-          } else {
-            if (checkFixedDeg(data)) {
-              return -1;
-            } else {
-              return 0;
-            }
-          }
-        }
-      }
+
       // Since deg of v is never used until the end,
       // we erase the values.
-      setDeg(data, v, -1);
+      setIndeg(data, v, -1);
+      setOutdeg(data, v, -1);
     }
     if (level == 1) {
       // If we come here, the edge set is empty (taking no edge).
